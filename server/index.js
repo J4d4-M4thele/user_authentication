9d6339af1c57ad86390404e74d4d95b3288fa82a
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-06-26 13:01:53 +0200

    update

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
-const PORT = 4000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log("Successfully connected to database!");
+})
+.catch(err => {
+    console.log("Error connecting to database: " + err);
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
 });
