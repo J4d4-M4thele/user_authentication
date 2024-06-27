f68f731b638da4ed85dfbd431c9d0ebc912750df
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-06-27 10:40:10 +0200

    update

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -2,3 +2,18 @@ const User = require('../models/userModel.js');
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
+module.exports.userVerification = (req, res) => {
+    const token = req.cookies.token;
+    if (!token) {
+        return res.json({ status: false });
+    }
+    jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
+        if (err) {
+            return res.json({ status: false });
+        } else {
+            const user = await User.findById(data.id);
+            if (user) return res.json({ status: true, user: user.username });
+            else return res.json({ status: false });
+        }
+    });
+};
