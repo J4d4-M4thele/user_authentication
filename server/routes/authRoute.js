c49f2532f8f812ab54b22e836d23bad352da8de4
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-06-27 10:56:02 +0200

    update

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
-const {signUp} = require('../controllers/authController.js');
+const { signUp, login } = require('../controllers/authController.js');
+const { userVerification } = require('../middleware/authMiddleware.js');
 
+router.post('/', userVerification);
 router.post('/signup', signUp);
+router.post('/login', login);
 
 module.exports = router;
