cf5a1251e04de1df66c7a79d63a04bfcbffa98bc
Jada Mathele <jadamaadimo.mathele@gmail.com>
2024-06-27 09:04:58 +0200

    adding schema

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -5,19 +5,19 @@ const userSchema = new mongoose.Schema({
     email: {
         type: String,
         required: [true, "Your email address is required"],
-        unique: true
+        unique: true,
     },
     username: {
         type: String,
-        required: [true, "Your username is required"]
+        required: [true, "Your username is required"],
     },
     password: {
         type: String,
-        required: [true, "Your password is required"]
+        required: [true, "Your password is required"],
     },
     createdAt: {
         type: Date,
-        default: new Date()
+        default: new Date(),
     }
 });
 
